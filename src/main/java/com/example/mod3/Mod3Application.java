023ea1724162f331cc0ba24a8a54244a91be82e3
cathe <enfedorko@gmail.com>
2026-08-29 20:08:11 +0300

    м

--- src/main/java/com/example/mod3/Mod3Application.java
+++ src/main/java/com/example/mod3/Mod3Application.java
@@ -1,8 +1,17 @@
 package com.example.mod3;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(
+        info=@Info(
+                title="Product API",
+                version= "1.0",
+                description ="API для управления продуктами"
+        )
+)
 @SpringBootApplication
 public class Mod3Application {
     public static void main(String[] args) {
--- src/main/java/com/example/mod3/ProductController.java
+++ src/main/java/com/example/mod3/ProductController.java
@@ -1,31 +1,53 @@
 package com.example.mod3;
 import org.springframework.web.bind.annotation.*;
-
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.List;
 
 @RestController
 @RequestMapping("/products")
+@Tag(name = "Products",description = "Работа с продуктами")
 public class ProductController {
     private final ProductService service;
 
     public ProductController(ProductService service){
         this.service = service;
     }
-
+    @Operation(summary = "Создать продукт",description = "Создаёт новый продукт")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200",description = "Продукт успешно создан"),
+            @ApiResponse(responseCode = "400",description = "Ошибка при создании продукта")
+    })
     @PostMapping
     public Product create(@RequestBody Product product){
         return service.create(product);
     }
-
+    @Operation(summary = "Получить все продукты",description = "Возвращает список всех продуктов")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200",description = "Список продуктов успешно получен")
+    })
     @GetMapping
     public List<Product> getAll(){
         return service.getAll();
     }
+    @Operation(summary = "Обновить продукт",description = "Изменяет данные продукта")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200",description = "Продукт успешно обновлён"),
+            @ApiResponse(responseCode = "404",description = "Продукт не найден")
+    })
     @PutMapping("/{id}")
+    @Parameter(description = "ID продукта",example = "1",required = true)
     public Product update(@PathVariable Long id, @RequestBody Product product){
         return service.update(id, product);
     }
-
+    @Operation(summary = "Удалить продукт",description = "Удаляет продукт по ID")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200",description = "Продукт успешно удалён"),
+            @ApiResponse(responseCode = "404",description = "Продукт не найден")
+    })
     @DeleteMapping("/{id}")
     public void delete(@PathVariable Long id){
         service.delete(id);
